6acbfa808299a104ac6ff3cc6a5c38d7ce810fe0
jtomassoni <jtomassoni@gmail.com>
2020-10-08 14:59:38 -0600

    have a working model of HTML and CSS, no towers yet

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+let towers = document.querySelectorAll('.towers');
+let towerA = document.querySelector('#towerA');
+let towerB = document.querySelector('#towerB');
+let towerC = document.querySelector('#towerC');
+let smallDisk = document.querySelector('#smallDisk');
+let mediumDisk = document.querySelector('#mediumDisk');
+let largeDisk = document.querySelector('#largeDisk');
+let playerMessage = document.querySelector('.playerMessage');
+let resetButton = document.querySelector('.resetButton');
+
+
+playerMessage.innerText = 'Welcome to Tower of Hanoi.  Rules are rules';
+let winningSolution = [smallDisk, mediumDisk, largeDisk];
+
+towerA = [];
+towerB = [];
+towerC = [];
