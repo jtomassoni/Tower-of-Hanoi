d03fe7a28f9255ec86d7d089a45768c5ee2755ba
jtomassoni <jtomassoni@gmail.com>
2020-10-08 15:21:03 -0600

    i've gotten some DOM commands working to change styles on clicked buttons, as well as change text in playerMessage to let you know which disk you've clicked.

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ let mediumDisk = document.querySelector('#mediumDisk');
 let largeDisk = document.querySelector('#largeDisk');
 let playerMessage = document.querySelector('.playerMessage');
 let resetButton = document.querySelector('.resetButton');
-
+let stacksButtons = document.querySelector('.stacksButtons');
+let moveToA = document.querySelector('#moveToA');
+let moveToB = document.querySelector('#moveToB');
+let moveToC = document.querySelector('#moveToC');
+let divButtons = document.querySelector('.diskButtons');
+// console.log(divButtons);
 
 playerMessage.innerText = 'Welcome to Tower of Hanoi.  Rules are rules';
 let winningSolution = [smallDisk, mediumDisk, largeDisk];
+// console.log(winningSolution);
 
 towerA = [];
 towerB = [];
 towerC = [];
+let currentDisk = null;
+
+divButtons.addEventListener('click', (event) => {
+	currentDisk = event;
+	playerMessage.innerText = `You've selected Disk ${currentDisk.target.value}, now select a tower to place it below.`;
+	console.log(currentDisk.target.classList.add('selected'));
+});
+
+let checkTower = null;
+stacksButtons.addEventListener('click', (event) => {
+	console.log(event);
+	checkTower = event;
+	//if the tower has a value in index[0] that's bigger than
+});
