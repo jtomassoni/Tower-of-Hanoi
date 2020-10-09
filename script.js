acf7968dcd52b725fd5dc538acc7f02cdafe47af
jtomassoni <jtomassoni@gmail.com>
2020-10-08 18:22:28 -0600

    my disk buttons now prevent anymore than one  selection

--- script.js
+++ script.js
@@ -11,44 +11,59 @@ let stacksButtons = document.querySelector('.stacksButtons');
 let moveToA = document.querySelector('#moveToA');
 let moveToB = document.querySelector('#moveToB');
 let moveToC = document.querySelector('#moveToC');
-let divButtons = document.querySelector('.diskButtons');
-console.log(divButtons);
+let diskButtons = document.querySelector('.diskButtons');
+
+// console.log(diskButtons);
 
 playerMessage.innerText = 'Welcome to Tower of Hanoi.  Rules are rules';
-let winningSolution = [smallDisk, mediumDisk, largeDisk];
-// console.log(winningSolution);
+
+let currentDisk = null;
+let incrementer = 0;
+
+diskButtons.addEventListener('click', (event) => {
+	//prevent action on the negative space
+	if (event.target.id === 'noClick') {
+		return true;
+	}
+	//prevent more than one disk from being selected
+	let buttonClasses = event.target.classList;
+	if (incrementer % 2 === 0) {
+		if (buttonClasses.value != 'selected') {
+			currentDisk = event;
+			playerMessage.innerText = `You've selected Disk ${currentDisk.target.value}, now select a tower to place it below.`;
+			currentDisk.target.classList.toggle('selected');
+		}
+	}
+	if (incrementer % 2 != 0) {
+		playerMessage.innerText = `You've already selected Disk ${currentDisk.target.value}, please select a tower to place it below.`;
+		return;
+	}
+	console.log(stacksButtons);
+	incrementer++;
+});
 
 towerA = [];
 towerB = [];
 towerC = [];
-let currentDisk = null;
-
-divButtons.addEventListener('click', (event) => {
-	console.log(event.target.id);
-	if (event.target.id != 'noClick') {
-		currentDisk = event;
-		playerMessage.innerText = `You've selected Disk ${currentDisk.target.value}, now select a tower to place it below.`;
-		currentDisk.target.classList.toggle('selected');
-	}
-});
-
+// console.log(towerA);
 let checkTower = null;
 
-stacksButtons.addEventListener('click', (event) => {
-	// console.log(event);
-	checkTower = event;
-	currentDisk.target.classList.remove('selected');
-});
+// stacksButtons.addEventListener('click', (event) => {
+//     checkTower = event;
+// 	console.log(checkTower.target.value);
+// });
 
-resetButton.addEventListener('click', (event) => {
-	// console.log(event);
-	playerMessage.innerText = 'Welcome to Tower of Hanoi.  Rules are rules';
-	console.log(divButtons);
-	// divButtons.classList.remove('selected');
-});
+// resetButton.addEventListener('click', (event) => {
+// 	// console.log(event);
+// 	playerMessage.innerText = 'Welcome to Tower of Hanoi.  Rules are rules';
+// 	console.log(diskButtons);
+// 	// diskButtons.classList.remove('selected');
+// });
 
 // const removeSelections = () => {
 // currentDisk.target.classList.remove('selected');
 // playerMessage.innerText =
 
 // }
+
+// let winningSolution = [smallDisk, mediumDisk, largeDisk];
