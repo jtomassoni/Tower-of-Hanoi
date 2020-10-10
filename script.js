a4a78058f9f5500eeba8437e843dca9eb5251aee
jtomassoni <jtomassoni@gmail.com>
2020-10-09 18:23:48 -0600

    pretty sure this is MVP

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let diskButtons = document.querySelector('.diskButtons');
 let moveCounter = document.querySelector('#moveCounter');
 let currentDisk = null;
 let counter = 1;
+let towerHeight = 0;
 
 smallDisk.addEventListener('click', (event) => {
 	const parent = event.target.parentElement;
@@ -28,7 +29,6 @@ smallDisk.addEventListener('click', (event) => {
 });
 mediumDisk.addEventListener('click', (event) => {
 	const parent = event.target.parentElement;
-	console.log(parent);
 	if (parent.childNodes[0].id === 'mediumDisk') {
 		currentDisk = event.target;
 		playerMessage.innerText = `Valid Selection: Move it to another tower`;
@@ -52,125 +52,94 @@ largeDisk.addEventListener('click', (event) => {
 });
 
 moveToA.addEventListener('click', (event) => {
-	if (currentDisk != null) {
+	const parent = event.target;
+	if (towerA.childNodes.length === 0) {
+		if (currentDisk != null) {
+			moveCounter.innerText = counter;
+			currentDisk.classList.toggle('selected');
+			counter++;
+			playerMessage.innerHTML =
+				'Select the next disk.  Make sure it is on <em>top</em> of the 	tower.';
+			towerA.prepend(currentDisk);
+			checkWin();
+		}
+	} else if (Number(currentDisk.value) < Number(towerA.childNodes[0].value)) {
 		towerA.prepend(currentDisk);
-		playerMessage.innerHTML =
-			'Select the next disk.  Make sure it is on <em>top</em> of the tower.';
 		moveCounter.innerText = counter;
-
 		currentDisk.classList.toggle('selected');
 		counter++;
 		currentDisk = null;
+		checkWin();
+	} else {
+		playerMessage.innerHTML =
+			'Invalid Selection:</br></br> <strong></strong>Larger disks</strong> can not be stacked on top of <strong>smaller disks.</strong>';
+		currentDisk.classList.toggle('selected');
 	}
 });
 moveToB.addEventListener('click', (event) => {
-	if (currentDisk != null) {
+	const parent = event.target;
+	if (towerB.childNodes.length === 0) {
+		if (currentDisk != null) {
+			moveCounter.innerText = counter;
+			currentDisk.classList.toggle('selected');
+			counter++;
+			playerMessage.innerHTML =
+				'Select the next disk.  Make sure it is on <em>top</em> of the 	tower.';
+			towerB.prepend(currentDisk);
+			checkWin();
+		}
+	} else if (Number(currentDisk.value) < Number(towerB.childNodes[0].value)) {
 		towerB.prepend(currentDisk);
-		playerMessage.innerHTML =
-			'Select the next disk.  Make sure it is on <em>top</em> of the tower.';
 		moveCounter.innerText = counter;
 		currentDisk.classList.toggle('selected');
 		counter++;
 		currentDisk = null;
+		checkWin();
+	} else {
+		playerMessage.innerHTML =
+			'Invalid Selection:</br></br> <strong></strong>Larger disks</strong> can not be stacked on top of <strong>smaller disks.</strong>';
+		currentDisk.classList.toggle('selected');
 	}
 });
 moveToC.addEventListener('click', (event) => {
-	if (currentDisk != null) {
+	const parent = event.target;
+	if (towerC.childNodes.length === 0) {
+		if (currentDisk != null) {
+			moveCounter.innerText = counter;
+			currentDisk.classList.toggle('selected');
+			counter++;
+			playerMessage.innerHTML =
+				'Select the next disk.  Make sure it is on <em>top</em> of the 	tower.';
+			towerC.prepend(currentDisk);
+			checkWin();
+		}
+	} else if (Number(currentDisk.value) < Number(towerC.childNodes[0].value)) {
 		towerC.prepend(currentDisk);
-		playerMessage.innerHTML =
-			'Select the next disk.  Make sure it is on <em>top</em> of the tower.';
 		moveCounter.innerText = counter;
 		currentDisk.classList.toggle('selected');
 		counter++;
 		currentDisk = null;
+		checkWin();
+	} else {
+		playerMessage.innerHTML =
+			'Invalid Selection:</br></br> <strong></strong>Larger disks</strong> can not be stacked on top of <strong>smaller disks.</strong>';
+		currentDisk.classList.toggle('selected');
 	}
 });
+
 resetButton.addEventListener('click', (event) => {
 	playerMessage.innerHTML = `...maybe do better this time?`;
 	towerA.prepend(largeDisk);
 	towerA.prepend(mediumDisk);
 	towerA.prepend(smallDisk);
-	currentDisk.classList.toggle('selected');
+	smallDisk.classList.remove('selected');
+	mediumDisk.classList.remove('selected');
+	largeDisk.classList.remove('selected');
+	moveCounter.innerText = '';
 	currentDisk = null;
 });
-
-// diskButtons.addEventListener('click', (event) => {
-// 	//prevent action on the negative space
-// 	if (event.target.id === 'noClick') {
-// 		return true;
-// 	}
-// 	//prevent more than one disk from being selected
-// 	//listens on the target, gets the value
-// 	//swaps it to a number
-// 	//and compares it to the value of the current value of
-// 	//only listen on 0index.
-// 	let buttonClasses = event.target.classList;
-// 	if (Number(event.target.value) === towerA[0].value) {
-// 		if (buttonClasses.value != 'selected') {
-// 			currentDisk = event;
-// 			playerMessage.innerText = `You've selected Disk ${currentDisk.target.value}, now select a tower to place it below.`;
-// 			currentDisk.target.classList.toggle('selected');
-// 			// console.log(currentDisk.target.value);
-// 			return currentDisk;
-// 		} else {
-// 			playerMessage.innerText = `You've already selected Disk ${currentDisk.target.value}, please select a tower to place it below.`;
-// 			return currentDisk;
-// 		}
-// 	}
-// 	return currentDisk;
-// });
-
-// //get move buttons to work the same as reset unless the disk is already in that array
-// moveToA.addEventListener('click', (event) => {
-// 	//if currentDisk hasn't been selected yet, tell the user to select a disk.
-// 	if (currentDisk === null) {
-// 		playerMessage.innerText =
-// 			"You haven't selected a disk to move yet.  Please select a disk before trying to move nothing into a tower.";
-// 	}
-// 	//now that we have a disk selected, we need to find out what tower array it's in.
-// 	console.log(Number(currentDisk.target.value));
-// 	console.log(towerA[0].value);
-// 	if (Number(currentDisk.target.value) === towerA[0].value) {
-// 		playerMessage.innerText =
-// 			"You can't move a disk to the tower it is already in.";
-// 	} else {
-// 		console.log('he');
-// 	}
-// 	currentDisk.target.classList.remove('selected');
-// });
-
-// moveToB.addEventListener('click', (event) => {
-// 	//if currentDisk hasn't been selected yet, tell the user to select a disk.
-// 	if (currentDisk === null) {
-// 		playerMessage.innerText =
-// 			"You haven't selected a disk to move yet.  Please select a disk before trying to move nothing into a tower.";
-// 	}
-// 	//now that we have a disk selected, we need to find out what tower array it's in.
-// 	console.log(Number(currentDisk.target.value));
-// 	// console.log(towerB[0].value);
-// 	if (towerB.length > Number(currentDisk.target.value)) {
-// 		playerMessage.innerText =
-// 			"You can't move a disk to the tower it is already in.";
-// 	} else {
-// 		console.log('he');
-// 		towerB.push(currentDisk.target.value);
-// 		towerB.currentDisk.target.classList.remove('selected');
-// 		return towerB;
-// 	}
-// });
-
-// //render the second tower if it's array is populated?
-
-// if (towerB.length > 0) {
-// 	console.log('were doing it');
-// }
-
-// //renders the first tower, NEEDS TO RENDER IT AS AN ARRAY
-// // startingCondition.forEach((element) => {
-// // 	console.log(element);
-// // 	let i = element.value;
-// // 	let button = document.createElement('button');
-// // 	button.id = `disk${i}`;
-// // 	button.value = i;
-// // 	diskButtons.appendChild(button);
-// // });
+function checkWin() {
+	if (towerC.childElementCount === 3) {
+		playerMessage.innerHTML = 'YOU WON!!!!';
+	}
+}
